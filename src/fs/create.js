2dcb9f437e30899065adc478286b4013c2bcd7be
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 12:06:28 +0200

    feat: Add nodejs-basics::create task implementation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises';
+import {assertFileNotExistsAsync, isFileExistsAsync, withCurrentFileMetaUrl} from "../utils/index.js";
+
+const {dirPath, filePath} = withCurrentFileMetaUrl(import.meta.url).getFileInDirPath('files', 'fresh.txt');
+
+/**
+ * @description The function that creates new file `fresh.txt` with content `I am fresh and young`inside the `files` folder;
+ * If file `fresh.txt` already exists - the `Error` with message `FS operation failed` will be thrown.
+ * @returns {Promise<void>}
+ */
+const create = async () =>
+    assertFileNotExistsAsync(filePath)
+        .then(() => isFileExistsAsync(dirPath))
+        .then((isFolderExists) => !isFolderExists ? fs.mkdir(dirPath) : undefined)
+        .then(() => fs.writeFile(filePath, 'I am fresh and young', {encoding: 'utf8'}));
 
 await create();
