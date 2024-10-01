fc2774478bc5b05bda7fca8ebb7ff7379b022eac
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 15:30:56 +0200

    feat: Add nodejs-basics::streams::transform task implementation

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import Stream from 'node:stream';
+
+const reverseStream = new Stream.Transform({
+    transform(chunk, _encoding, callback) {
+        this.push(String(chunk).split('').reverse().join(''));
+        callback();
+    }
+});
+
+/**
+ * @description The function that reads data from `process.stdin`, reverses text using `Transform Stream` and then writes it into `process.stdout`
+ * @returns {Promise<void>}
+ */
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
