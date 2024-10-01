b1c7c7ad892c7b074b703e913d5bb2ba21d5d1b8
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 12:09:00 +0200

    feat: Add nodejs-basics::fs::delete task implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises';
+import {assertFileExistsAsync, withCurrentFileMetaUrl} from "../utils/index.js";
+
+const {filePath} = withCurrentFileMetaUrl(import.meta.url).getFileInDirPath('files', 'fileToRemove.txt');
+
+/**
+ * @description The function that deletes file `fileToRemove.txt`;
+ * If there's no file `fileToRemove.txt` - the `Error` with message `FS operation failed` will be thrown;
+ * @returns {Promise<void>}
+ */
+const remove = async () => assertFileExistsAsync(filePath).then(() => fs.rm(filePath));
 
 await remove();
