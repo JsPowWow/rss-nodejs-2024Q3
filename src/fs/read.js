9516064d2d065646a5e242ef485f5f219c60076a
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 12:10:44 +0200

    feat: Add nodejs-basics::fs::read task implementation

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'node:fs/promises';
+import {assertFileExistsAsync, withCurrentFileMetaUrl} from "../utils/index.js";
+
+const {filePath} = withCurrentFileMetaUrl(import.meta.url).getFileInDirPath('files', 'fileToRead.txt');
+
+/**
+ * @description The function that prints content of the `fileToRead.txt` into console;
+ * If there's no file `fileToRead.txt` - the `Error` with message `FS operation failed` will be thrown;
+ * @returns {Promise<void>}
+ */
+const read = async () =>
+    assertFileExistsAsync(filePath)
+        .then(() => fs.readFile(filePath, {encoding: 'utf8'}))
+        .then(console.log);
 
 await read();
