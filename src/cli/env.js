217b6f096ce2f7c4cc4dd3226bcedd9d56dac3e1
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 12:38:49 +0200

    feat: Add nodejs-basics::cli::env task implementation

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+/**
+ * @description The function that parses environment variables with prefix `RSS_` and prints them to the console
+ * in the format `RSS_name1=value1; RSS_name2=value2`.
+ */
 const parseEnv = () => {
-    // Write your code here 
+    console.log(Object.entries(process.env)
+        .filter(([envName]) => envName.startsWith("RSS_"))
+        .map(([envName, envValue]) => `${envName}=${envValue}`)
+        .join("; "));
 };
 
 parseEnv();
