56eec4115e4b41c6db01cad82a0d757cb16f762d
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-01 15:16:15 +0200

    feat: Add nodejs-basics::streams::write task implementation

--- src/streams/read.js
+++ src/streams/read.js
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import {withCurrentFileMetaUrl} from "../utils/index.js";
 
 const {filePath} = withCurrentFileMetaUrl(import.meta.url).getFileInDirPath('files', 'fileToRead.txt');
+
 /**
  * @description The function that reads file `fileToRead.txt` content using Readable Stream and prints it's content into `process.stdout`
  * @returns {Promise<void>}
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+import {withCurrentFileMetaUrl} from "../utils/index.js";
+
+const {filePath} = withCurrentFileMetaUrl(import.meta.url).getFileInDirPath('files', 'fileToWrite.txt');
+
+/**
+ * @description The function that writes `process.stdin` data into file `fileToWrite.txt` content using Writable Stream
+ * @returns {Promise<void>}
+ */
 const write = async () => {
-    // Write your code here 
+    process.stdin.pipe(fs.createWriteStream(filePath));
 };
 
 await write();
