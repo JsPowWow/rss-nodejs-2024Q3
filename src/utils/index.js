474430ed4a008b1b2e7d13ee7212fe6496082ae1
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-03 18:45:58 +0200

    chore: Add project commonUtils.js

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+export {
+    identity,
+    sleep,
+    noop,
+    isSome,
+    isSomeFunction,
+    isNil,
+    assertIsNonNullable,
+    isInstanceOf,
+    assertIsInstanceOf,
+    stubTrue,
+    stubFalse,
+    isOddNumber,
+    isEvenNumber
+} from './commonUtils.js';
