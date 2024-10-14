728381268b5f0cbd53337ce774d3d7b35b5711aa
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-14 10:20:38 +0200

    bug: Fix cli command -> "cd"

--- src/cmdShell/commands/cd.js
+++ src/cmdShell/commands/cd.js
@@ -1,13 +1,19 @@
 import {chdir} from 'node:process'
 import {getCurrentWorkingDir, parseInputForHelpOption} from '#shell-utils';
 import {Nothing, pipeAsync} from '#fp-utils';
-import {OperationFailedError} from '#shell-errors';
+import {assertHasExpectedPositionalsNum, OperationFailedError} from '#shell-errors';
 import {outputMsg} from '#shell-messages';
 import path from 'node:path';
 
 const COMMAND_DESCRIPTION = outputMsg`Go to dedicated folder from current directory (${'path_to_directory'} can be relative or absolute)`;
 
 const parseInput = parseInputForHelpOption;
+
+/**
+ * @param {string} path
+ * @return {string}
+ */
+const getAsDirectory = (path) => path.endsWith("/") || path.endsWith("\\") ? path : `${path}/`
 
 /** @implements {Command} */
 export default class GoToDirectoryCommand {
@@ -24,14 +30,15 @@ export default class GoToDirectoryCommand {
      * @returns {AsyncGenerator<CmdOperation, void, *>}
      */
     async* execute(ctx) {
-        const {values, positionals} = await parseInput(ctx);
-        ctx.debug ? yield {type: 'debug', message: 'parsed arguments', data: {values, positionals}} : Nothing;
+        const parsedArgs = await parseInput(ctx);
+        ctx.debug ? yield {type: 'debug', message: 'parsed arguments', data: parsedArgs} : Nothing;
 
-        if (values['help']) {
+        if (parsedArgs.values['help']) {
             return yield {type: 'success', message: ctx.input, data: outputMsg`${GoToDirectoryCommand.description}`};
         }
+        assertHasExpectedPositionalsNum(GoToDirectoryCommand.command, 1, parsedArgs);
 
-        const newDirPath = ctx.input.trimStart().slice(GoToDirectoryCommand.command.length + 1);
+        const newDirPath = path.resolve(getAsDirectory(parsedArgs.positionals[0]));
 
         await pipeAsync(chdir)(newDirPath).catch(OperationFailedError.reThrowWith);
 
