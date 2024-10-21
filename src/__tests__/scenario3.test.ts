ca48e6009bc8bd103e9d7d79188d164b052167ac
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2024-10-21 16:19:45 +0200

    feat: Add scenario3.test(s)

---
+++ src/__tests__/scenario3.test.ts
@@ -0,0 +1,67 @@
+import request from 'supertest';
+
+import { enableConsoleLogging, getUserBody, startTestServers } from '../UsersService/__tests__/test-utils';
+
+describe('DBService tests', () => {
+  const { usersServer, memDbServer, stopUsersServer, stopMemDbServer } = startTestServers(777);
+  beforeEach(async () => {
+    enableConsoleLogging(false);
+    await request(memDbServer).delete('/api/purge');
+  });
+
+  afterAll(async () => {
+    await stopMemDbServer();
+    await stopUsersServer();
+  });
+
+  /**
+   * Scenario #3
+   * 1. Get all records with a GET api/users request (an empty array is expected)
+   * 2. A new object is created by a POST api/users request (a response containing newly created record expected)
+   * 3. A new another object is created by a POST api/users request (a response containing newly created record expected)
+   * 4. With a GET api/users/ request, we try to get the created records (the created records expected)
+   * 5. With a PURGE api/records/ request, we try to purge the created records
+   * 6. With a GET api/users/ request, we try to get the created records (the created records are purged, no records expected)
+   */
+  it(`"Scenario #1 should pass successfully`, async () => {
+    // 1. Get all records with a GET api/users request (an empty array is expected)
+    const { body: usersInitial } = await request(usersServer)
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', 'application/json');
+
+    expect(usersInitial).toStrictEqual([]);
+
+    // 2. A new object is created by a POST api/users request (a response containing newly created record is expected)
+    const { body: user1 } = await request(usersServer)
+      .post('/api/users')
+      .send(getUserBody({ name: 'Alex', age: 33 }, 'anime'));
+    expect(user1).toStrictEqual({ id: expect.any(String), name: 'Alex', age: 33, hobbies: ['anime'] });
+
+    // 3. A new another object is created by a POST api/users request (a response containing newly created record is expected)
+    const { body: user2 } = await request(usersServer)
+      .post('/api/users')
+      .send(getUserBody({ name: 'Olga', age: 18 }, 'shopping'));
+    expect(user2).toStrictEqual({ id: expect.any(String), name: 'Olga', age: 18, hobbies: ['shopping'] });
+
+    // 4. With a GET api/users/ request, we try to get the created records (the created records are expected)
+    const { body: users } = await request(usersServer)
+      .get(`/api/users`)
+      .expect(200)
+      .expect('Content-Type', 'application/json');
+    expect(users).toStrictEqual([
+      { id: user1.id, name: 'Alex', age: 33, hobbies: ['anime'] },
+      { age: 18, hobbies: ['shopping'], id: user2.id, name: 'Olga' },
+    ]);
+
+    // 5. With a PURGE api/records/ request, we try to purge the created records
+    await request(memDbServer).delete(`/api/purge`).expect(200);
+
+    // 6. With a GET api/users/ request, we try to get the created records (the created records are purged, no records expected)
+    const { body: usersPurged } = await request(usersServer)
+      .get(`/api/users`)
+      .expect(200)
+      .expect('Content-Type', 'application/json');
+    expect(usersPurged).toStrictEqual([]);
+  });
+});
